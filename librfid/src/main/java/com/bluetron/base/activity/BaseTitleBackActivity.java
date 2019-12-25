43e0f40730418602925b24f658322c316cc683e6
tongxb <89597781@qq.com>
2019-12-25 15:08:57 +0800

    添加默认dialog

---
+++ librfid/src/main/java/com/bluetron/base/activity/BaseTitleBackActivity.java
@@ -0,0 +1,32 @@
+package com.bluetron.base.activity;
+
+import android.widget.TextView;
+
+import com.bluetron.librfid.R;
+
+/**
+ * @author tongxb
+ * @date 2018/5/9
+ */
+
+public abstract class BaseTitleBackActivity extends BaseBackActivity {
+    protected TextView commonTvTitle;
+
+    @Override
+    protected void inflateBaseView() {
+        super.inflateBaseView();
+        commonTvTitle = findViewById(R.id.common_tv_title);
+    }
+
+    public void setTitleTxt(String title) {
+        commonTvTitle.setText(title);
+    }
+
+    public void setTitleTxt(int resId) {
+        commonTvTitle.setText(resId);
+    }
+
+    public void setTitleVisibility(int visibility) {
+        commonTvTitle.setVisibility(visibility);
+    }
+}
